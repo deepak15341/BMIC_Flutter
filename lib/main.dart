ed82163564510082401e0d68d4b5ed8c6b819672
deepak15341 <deepakkumarsahbgp123@gmail.com>
2023-10-11 19:57:02 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'MyHomePage.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -27,30 +29,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text("Bmi Calculator"),
-      ),
-
-    );
-  }
-}
