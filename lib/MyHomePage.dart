7bda42eec934612f7a718d1ac95b97f7735d3800
deepak15341 <deepakkumarsahbgp123@gmail.com>
2023-10-12 07:56:10 +0530

    initial

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -11,19 +10,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+           title: Text(widget.title),
+      ),
+           body: const Column(
+              children: [
+             Expanded(
+               child: Row(
+                 children: [
+                   Reusable(),
+                   Reusable(),
+                 ],
+               ),
+             ),
+                Expanded(child: Row(
+                  children: [
+                    Reusable(),
+                  ],
+                )),
+                Expanded(child: Row(
+                  children: [
+                    Reusable(),
+                    Reusable(),
+                  ],
+                ))
+        ],
+      ),
+    ));
+  }
+}
 
+class Reusable extends StatelessWidget{
+  const Reusable({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text("Bmi Calculator"),
-      ),
-
-    );
+return Expanded(
+  child: Container(
+    margin: const EdgeInsets.all(15),
+    decoration: BoxDecoration(
+        color: const Color(0xff1D1E33),
+        borderRadius: BorderRadius.circular(10.0)),
+  ),
+);
   }
+
 }
